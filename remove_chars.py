81766b5887d943ce6cffac32640987ddc9497472
Kate Gonzaga <97600695+kateg07@users.noreply.github.com>
2025-12-30 20:35:08 +0800

    Delete remove_chars.py

--- remove_chars.py
+++
@@ -1,9 +0,0 @@
-
-def remove_chars(word, n):
-    result = ""
-    print("Original word:", word)
-    for i in range(len(word)):
-        result = word[n:]
-    print("'",result,"'", "first", n, "characters removed")
-
-remove_chars("pynative", 4)
